6f3204e03505a66589207c2b21bf6e3173627229
Akash <akashyoungstar@gmail.com>
2025-06-03 18:25:40 +0530

    api /

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ import toolRoutes from "./routes/toolRoutes";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const CLIENT_URL ="https://practice-tool-client.vercel.app/";
+const CLIENT_URL ="https://practice-tool-client.vercel.app";
 
 
 
